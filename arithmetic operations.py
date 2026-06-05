2ea57eeb0d825c25d962c81a00ef2a62b8986a09
Nanhisinghmar <kajalsinghmar80@gmail.com>
2026-06-05 20:35:51 +0530

    Add files via upload

---
+++ arithmetic operations.py
@@ -0,0 +1,36 @@
+#Arithmetic operations
+
+#Addition
+a = 10
+b = 5
+print("Sum =", a + b)
+
+#Subtraction
+a = 10
+b = 5
+print("Difference =", a - b)
+ 
+#Multiplication
+a = 10
+b = 5
+print("Product =", a * b)
+
+#Division
+a = 10
+b = 5
+print("Quotient =", a / b)
+
+#Modulus
+a = 10
+b = 3
+print("Remainder =", a % b)
+
+#Power
+a = 2
+b = 3
+print("Power =", a ** b)
+
+#Floor Division
+a = 10
+b = 3
+print("Floor Division =", a // b)
